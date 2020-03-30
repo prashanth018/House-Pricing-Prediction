2236598c4cbc912cd1f2abe49a0b3a0f8a55c098
Prashanth Duvvuri <prashanthsedan@gmail.com>
2020-03-31 00:41:05 +0530

    Have added methods to normalize, added documentation, added the clean train and test file to data directory.

--- data.py
+++ data.py
@@ -50,15 +50,21 @@ class Data:
         self.replace()
         # Replace NAN values observed in continuous features
         self.fillna_features(self.CONTINUOUS_FEATURES_WITH_NAN, op="median")
+        #        self.normalize(self.CONTINUOUS_FEATURES)
+        self.normalize(self.features)
+        self.clean()
 
     # Loads the train and test dataframes with the files present int the given path.
     def load_data(self, train_path, test_path):
         self.df_train = pd.read_csv(train_path)
         self.df_test = pd.read_csv(test_path)
 
+    # Gets the features.
     def load_features(self):
         return self.df_test.keys()
 
+    # Method to display the unique values corresponding to the set of features specified
+    # to the method. Considers all the features if nothing specified to the method.
     def find_unique_values(self, features=[]):
         if len(features) == 0:
             features = self.features
@@ -66,6 +72,7 @@ class Data:
             classes = self.df_combined[key].unique()
             print(key, classes.shape, classes)
 
+    # Method to find the number of missing values for all the features.
     def find_features_with_nan(self):
         for key in self.features:
             classes = self.df_combined[key]
@@ -73,10 +80,18 @@ class Data:
                 print(key, str(classes.isna().sum()) + "/" + str(classes.shape[0]))
 
     # Given any column and the dataframe to this method, the method returns the dataframe
-    # with the corresponding column normalized.
-    def normalize(self, df, column_name):
-        return
+    # with the corresponding column normalized. Uses min-max normalization.
+    def normalize(self, features_to_normalize):
+        features_to_normalize = list(features_to_normalize)
+        for idx in range(len(features_to_normalize)):
+            if features_to_normalize[idx] == 'Id':
+                del features_to_normalize[idx]
+                break
 
+        self.df_combined[features_to_normalize] = self.df_combined[features_to_normalize].apply(
+            lambda x: (x - x.min()) / (x.max() - x.min()))
+
+    # given a list of features in input.txt, the method give a list of these features.
     def get_list(self):
         f = open("input.txt", "r")
         ls = f.readlines()
@@ -85,6 +100,9 @@ class Data:
         print(ls)
         f.close()
 
+    # Method to create a map for the values of each categorical features to numbers. Includes
+    # functionality to segregate the features with genuine NA values from the features with
+    # few missing records. Populates the feature_map class attribute.
     def create_tables(self):
         for w in self.CATEGORICAL_FEATURES:
             self.feature_map[w] = {}
@@ -98,6 +116,8 @@ class Data:
                 self.feature_map[w][vals[val_idx]] = val_idx
         # pprint(self.feature_map)
 
+    # Given a list of attributes, method populates the missing records with the either mean/median
+    # if specified, else uses mode as the default operation.
     def fillna_features(self, ExtArr, op="mode"):
         for w in ExtArr:
             if op == "mode":
@@ -107,8 +127,22 @@ class Data:
             elif op == "mean":
                 self.df_combined[w].fillna(self.df_combined[w].mean(), inplace=True)
 
+    # Method to selectively replace the values of the attributes in the dataframe with the values
+    # corresponding to the attributes specified in the feature_map.
     def replace(self):
         self.df_combined.replace(self.feature_map, inplace=True)
+
+    def clean(self):
+        train_rows = self.df_train.shape[0]
+        test_rows = self.df_test.shape[0]
+        self.df_train[self.features[:80]] = self.df_combined[:train_rows]
+        self.df_test = self.df_combined[-test_rows:]
+        print(self.df_train.shape)
+        print(self.df_test.shape)
+
+    def save_to_file(self):
+        self.df_train.to_csv("cleaned_train.csv")
+        self.df_test.to_csv("cleaned_test.csv")
 
 
 if __name__ == "__main__":
@@ -122,4 +156,8 @@ if __name__ == "__main__":
     # print("*************************************************************************************")
     # dp.find_unique_values(dp.CONTINUOUS_FEATURES_WITH_NAN)
     # dp.replace()
-    dp.find_unique_values()
+    # dp.find_unique_values()
+    # dp.find_unique_values()
+    # dp.clean()
+    # print(dp.df_test)
+    dp.save_to_file()
